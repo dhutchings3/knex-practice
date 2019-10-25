8e32b5e4519be2f644a6c42d3526f935c2f61b05
Danielle Marie <dhutchings36@yahoo.com>
2019-10-25 17:38:43 -0500

    first commit

---
+++ src/store.js
@@ -0,0 +1,16 @@
+const uuid = require('uuid/v4')
+
+const bookmarks = [
+  { id: uuid(),
+    title: 'Thinkful',
+    url: 'https://www.thinkful.com',
+    description: 'Think outside the classroom',
+    rating: 5 },
+  { id: uuid(),
+    title: 'Google',
+    url: 'https://www.google.com',
+    description: 'Where we find everything else',
+    rating: 4 }
+]
+  
+  module.exports = { bookmarks }
