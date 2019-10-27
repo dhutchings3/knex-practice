d8d3c5ab5f6849c9ed2d681ba1845dff8a5f248d
Danielle Marie <dhutchings36@yahoo.com>
2019-10-27 16:55:17 -0500

    updated and added blogful files

---
+++ blogful-scripts/seed.blogful.sql
@@ -0,0 +1,23 @@
+INSERT INTO blogful_articles (title, content, date_published)
+VALUES
+    ('Resume tips', 'A handful of resume tips to help you get hired',  now() - '5 days'::INTERVAL),
+    ('Job search strategy', 'How to approach your job search',  now() - '2 days'::INTERVAL),
+    ('All about Node.js', 'A beginners guide to Node.js',  now() - '5 days'::INTERVAL),
+    ('Online Portfolio', 'Why you should have one and how to get started',  now() - '23 days'::INTERVAL),
+    ('Javascript tricks', 'Useful tricks to help you learn JS',  now() - '7 days'::INTERVAL),
+    ('HTML and CSS Overvview', 'All about how to get started with html and css',  now() - '5 days'::INTERVAL),
+    ('jQuery fun', 'How to incorporate jQuery into your code',  now() - '3 days'::INTERVAL),
+    ('Learning React', 'Some of the more difficult React concepts and how to conquer them',  now() - '2 days'::INTERVAL),
+    ('Learning to code', 'Why learning to code is important for any career',  now() - '8 days'::INTERVAL),
+    ('React intro', 'A guide to get you started with React.js',  now() - '7 days'::INTERVAL),
+    ('The power of networking', 'How netowrking can help you in your job search',  now() - '11 days'::INTERVAL),
+    ('Personal branding', 'A guide to what personal braning is and why it is important',  now() - '13 days'::INTERVAL),
+    ('Coding challenges', 'Why you should incorporate them into your learning',  now() - '20 days'::INTERVAL),
+    ('Study Breaks', 'How they can help you to stay focus and on track when learning something new',  now() - '4 days'::INTERVAL),
+    ('CSS tricks', 'A few cool CSS tricks to learn',  now() - '5 days'::INTERVAL),
+    ('Building a website', 'A quick tutorial on building a website',  now() - '5 days'::INTERVAL),
+    ('Hello world', 'Where did it start',  now() - '14 days'::INTERVAL),
+    ('The right culture for you', 'All about company culture and the importance it plays in job satisfaction',  now() - '6 days'::INTERVAL),
+    ('React Study Guide', 'An overview of some of the main React concepts',  now() - '11 days'::INTERVAL),
+    ('Beginner Resources', 'A guide on some helpful resources for learning to code',  now() - '2 days'::INTERVAL)
+;
